4b86ee37dcbc0c17714baa9b76d835548c9a29e0
gozielaw <55288978+gozielaw@users.noreply.github.com>
2020-10-01 13:12:25 +0100

    second commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="columns m-0">
-    <div class="column first m-0 pt-6">
-      <p class="title head">
-        slot here
-      </p>
-      <img src="../assets/login.png" width="500px">
-    </div>
-    <div class="column">
-      Second column
-    </div>
-  </div>
+  <enter-page>
+    <template v-slot:head-text>
+      Login Here
+    </template>
+  </enter-page>
 </template>
 
+<script>
+import EnterPage from '../components/EnterPage'
+
+export default {
+  components: {
+    EnterPage
+  }
+}
+</script>
 <style lang="scss" scoped>
-.first {
-  left: 0px;
-  text-align: center;
-  height: 100vh;
-  background: linear-gradient(180deg, #4612D1 36.04%, #12D18C 121.28%);
-}
-.head {
-  color: #ffffff;
-  margin: 6rem auto 3rem;
-}
+
 </style>
