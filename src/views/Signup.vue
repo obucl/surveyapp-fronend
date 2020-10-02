a808f3862bbb237b4958aa01bc4c05ff9d1c701c
gozielaw <55288978+gozielaw@users.noreply.github.com>
2020-10-02 11:01:17 +0100

    signup add

---
+++ src/views/Signup.vue
@@ -0,0 +1,20 @@
+<template>
+  <enter-page>
+    <template v-slot:head-text>
+      Signup Here
+    </template>
+  </enter-page>
+</template>
+
+<script>
+import EnterPage from '../components/EnterPage'
+
+export default {
+  components: {
+    EnterPage
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
